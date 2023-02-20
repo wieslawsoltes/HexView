e53871a6fa0cdf0b6bca6a87116611d3d3b52ee7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-20 12:57:40 +0100

    Update MainWindow.axaml.cs

--- HexView/MainWindow.axaml.cs
+++ HexView/MainWindow.axaml.cs
@@ -9,12 +9,17 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    protected override void OnLoaded()
+    {
+        base.OnLoaded();
 
         // var path = @"/Users/wieslawsoltes/Documents/GitHub/Acdparser/clippitMS/CLIPPIT.ACS";
         var path = @"c:\Users\Administrator\Documents\GitHub\Acdparser\clippitMS\CLIPPIT.ACS";
 
         var hexViewState = new HexViewState(path);
-
+/*
         // var startLine = 0;
         // var endLine = 10;
         var startLine = hexViewState.Lines - 10;
@@ -27,8 +32,10 @@ public partial class MainWindow : Window
             sb.AppendLine();
         }
         Console.WriteLine(sb);
-
+*/
         HexViewControl.State = hexViewState;
         HexViewControl.InvalidateScrollable();
     }
+    
+    
 }
