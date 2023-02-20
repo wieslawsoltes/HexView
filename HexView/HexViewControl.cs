d02fba6f068bfdd5b8f3796a0c18fda0b122750d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-20 12:57:31 +0100

    Update HexViewControl.cs

--- HexView/HexViewControl.cs
+++ HexView/HexViewControl.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Globalization;
+using System.Text;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Documents;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Media;
@@ -17,7 +20,7 @@ public class HexViewControl : Control, ILogicalScrollable
     private bool _canVerticallyScroll;
     private EventHandler? _scrollInvalidated;
 
-    public HexViewState State { get;  set; }
+    public HexViewState? State { get;  set; }
 
     Size IScrollable.Extent => _extent;
 
@@ -67,9 +70,11 @@ public class HexViewControl : Control, ILogicalScrollable
         remove => _scrollInvalidated -= value;
     }
 
-    Size ILogicalScrollable.ScrollSize => new Size(1, 1);
+    // TODO: Use LineHeight
+    Size ILogicalScrollable.ScrollSize => new Size(1, 14);
 
-    Size ILogicalScrollable.PageScrollSize => new Size(10, 10);
+    // TODO: Use LineHeight
+    Size ILogicalScrollable.PageScrollSize => new Size(10,14);
 
     bool ILogicalScrollable.BringIntoView(Control target, Rect targetRect)
     {
@@ -93,24 +98,63 @@ public class HexViewControl : Control, ILogicalScrollable
             return;
         }
 
+        var lineHeight = 14;
+        var lines = State?.Lines ?? 0;
+
         var width = Bounds.Width;
         var height = Bounds.Height;
         var viewport = new Size(width, height);
-        var extent = new Size(width, 14 * 1000); // Text height * 1000 lines
-        var offset = new Vector(0, 0);
+        var extent = new Size(width, lineHeight * lines); // Text height * 1000 lines
+        //var offset = new Vector(0, 0);
 
-        Console.WriteLine($"{Bounds.Width} {Bounds.Height} {_offset}");
+        //Console.WriteLine($"{Bounds.Width} {Bounds.Height} {_offset}");
         _extent = extent;
         //_offset = offset;
         _viewport = viewport;
 
         scrollable.RaiseScrollInvalidated(EventArgs.Empty);
+        
+        InvalidateVisual();
     }
 
     public override void Render(DrawingContext context)
     {
         base.Render(context);
+
+        if (State is null)
+        {
+            return;
+        }
+
+        var lineHeight = 14;
+
+        var startLine = (long)(_offset.Y / lineHeight);
+        var endLine = (long)(startLine + _viewport.Height / lineHeight);
         
+        //Console.WriteLine($"Render {startLine}..{endLine}");
         
+        var sb = new StringBuilder();
+        for (var i = startLine; i <= endLine; i++)
+        {
+            var bytes = State.GetLine(i);
+            State.AddLine(bytes, i, sb);
+            sb.AppendLine();
+        }
+
+        var text = sb.ToString();
+
+        var typeface = new Typeface(TextElement.GetFontFamily(this));
+
+        var ft = new FormattedText(
+            text,
+            CultureInfo.CurrentCulture,
+            FlowDirection.LeftToRight,
+            typeface,
+            lineHeight,
+            Brushes.Black);
+
+        var origin = new Point();
+
+        context.DrawText(ft, origin);
     }
 }
