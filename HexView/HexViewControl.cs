ca820df7cc03fb5d00e3311d24cf93f96752348e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-20 10:44:52 +0100

    Create HexViewControl.cs

---
+++ HexView/HexViewControl.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Media;
+
+namespace HexView;
+
+public class HexViewControl : Control, ILogicalScrollable
+{
+    private volatile bool _updating = false;
+    private Size _extent;
+    private Size _viewport;
+    private Vector _offset;
+    private bool _canHorizontallyScroll;
+    private bool _canVerticallyScroll;
+    private EventHandler? _scrollInvalidated;
+
+    public HexViewState State { get;  set; }
+
+    Size IScrollable.Extent => _extent;
+
+    Vector IScrollable.Offset
+    {
+        get => _offset;
+        set
+        {
+            if (_updating)
+            {
+                return;
+            }
+            _updating = true;
+            _offset = value;
+            InvalidateScrollable();
+            _updating = false;
+        }
+    }
+
+    Size IScrollable.Viewport => _viewport;
+
+    bool ILogicalScrollable.CanHorizontallyScroll
+    {
+        get => _canHorizontallyScroll;
+        set
+        {
+            _canHorizontallyScroll = value;
+            InvalidateMeasure();
+        }
+    }
+
+    bool ILogicalScrollable.CanVerticallyScroll
+    {
+        get => _canVerticallyScroll;
+        set
+        {
+            _canVerticallyScroll = value;
+            InvalidateMeasure();
+        }
+    }
+
+    bool ILogicalScrollable.IsLogicalScrollEnabled => true;
+
+    event EventHandler? ILogicalScrollable.ScrollInvalidated
+    {
+        add => _scrollInvalidated += value;
+        remove => _scrollInvalidated -= value;
+    }
+
+    Size ILogicalScrollable.ScrollSize => new Size(1, 1);
+
+    Size ILogicalScrollable.PageScrollSize => new Size(10, 10);
+
+    bool ILogicalScrollable.BringIntoView(Control target, Rect targetRect)
+    {
+        return false;
+    }
+
+    Control? ILogicalScrollable.GetControlInDirection(NavigationDirection direction, Control? from)
+    {
+        return null;
+    }
+
+    void ILogicalScrollable.RaiseScrollInvalidated(EventArgs e)
+    {
+        _scrollInvalidated?.Invoke(this, e);
+    }
+
+    public void InvalidateScrollable()
+    {
+        if (this is not ILogicalScrollable scrollable)
+        {
+            return;
+        }
+
+        var width = Bounds.Width;
+        var height = Bounds.Height;
+        var viewport = new Size(width, height);
+        var extent = new Size(width, 14 * 1000); // Text height * 1000 lines
+        var offset = new Vector(0, 0);
+
+        Console.WriteLine($"{Bounds.Width} {Bounds.Height} {_offset}");
+        _extent = extent;
+        //_offset = offset;
+        _viewport = viewport;
+
+        scrollable.RaiseScrollInvalidated(EventArgs.Empty);
+    }
+
+    public override void Render(DrawingContext context)
+    {
+        base.Render(context);
+        
+        
+    }
+}
