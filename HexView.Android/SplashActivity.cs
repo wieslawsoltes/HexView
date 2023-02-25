b116b47d49bb3c660d69eb14adb871df7f494484
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-25 16:34:14 +0100

    Split

---
+++ HexView.Android/SplashActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Content;
+using Avalonia.Android;
+
+namespace HexView.Android;
+
+[Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+public class SplashActivity : AvaloniaSplashActivity<App>
+{
+    protected override void OnResume()
+    {
+        base.OnResume();
+
+        StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        Finish();
+    }
+}
